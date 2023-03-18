f8fb4bc09a24b363153f56b2c20c483779dca96d
Daniel González Briseño <qbixmex@gmail.com>
2023-03-18 16:25:41 -0600

    manage-signup-errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -8,6 +8,8 @@ import { CreateUserInput, UpdateUserInput } from './dto';
 
 @Injectable()
 export class UsersService {
+
+  private logger = new Logger('UsersService');
 
   constructor(
     @InjectRepository(User)
@@ -27,12 +29,22 @@ export class UsersService {
       const newUser = this.usersRepository.create(signUpInput);
       return await this.usersRepository.save(newUser);
     } catch (error) {
-      console.log(error);
-      throw new BadRequestException('Something went wrong');
+      this.handleDBErrors(error);
     }
   }
 
   async block(id: string): Promise<User> {
     throw new Error(`block method not implemented`);
   }
+
+  private handleDBErrors( error: any ): never {
+    this.logger.error(error);
+
+    if (error.code === '23505') {
+      throw new BadRequestException( error.detail.replace('Key ', '') );
+    }
+
+    throw new InternalServerErrorException('Please check log errors');
+  }
+
 }
