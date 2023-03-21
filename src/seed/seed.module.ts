e608ea6f955b559a96dbb5c18a82c210d71ae01b
Daniel González Briseño <qbixmex@gmail.com>
2023-03-21 00:20:38 -0600

    delete-records

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { UsersModule } from '../users/users.module';
+import { ItemsModule } from '../items/items.module';
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ ConfigModule, UsersModule, ItemsModule ],
   providers: [SeedResolver, SeedService]
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Item } from '../items/entities';
+import { User } from '../users/entities';
 
 @Injectable()
 export class SeedService {
@@ -7,9 +12,17 @@ export class SeedService {
   private isProd: boolean;
 
   constructor(
-    private readonly configService: ConfigService
+
+    private readonly configService: ConfigService,
+
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
   ) {
-    this.isProd = configService.get('STATE') == 'prod';
+    this.isProd = this.configService.get('STATE') == 'prod';
   }
 
   async executeSeed(): Promise<boolean> {
@@ -19,11 +32,27 @@ export class SeedService {
       throw new UnauthorizedException('We cannot run SEED on Production!');
     }
 
-    // TODO: Clear Database
+    // Clear Database
+    await this.deleteDatabase();
+
     // TODO: Create Users
     // TODO: Create Items
 
     return true;
+  }
+
+  async deleteDatabase(): Promise<void> {
+    //* Delete Items
+    await this.itemsRepository.createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+
+    // Delete Users
+    await this.usersRepository.createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 
 }
