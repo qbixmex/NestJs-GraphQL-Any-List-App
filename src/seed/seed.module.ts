d26777ed3a758ea6402b0210e9cacde4600861e4
Daniel González Briseño <qbixmex@gmail.com>
2023-03-21 00:10:06 -0600

    seed-protection

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
 
 @Module({
+  imports: [ConfigModule],
   providers: [SeedResolver, SeedService]
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
 
-  // constructor() {}
+  private isProd: boolean;
+
+  constructor(
+    private readonly configService: ConfigService
+  ) {
+    this.isProd = configService.get('STATE') == 'prod';
+  }
 
   async executeSeed(): Promise<boolean> {
+
+    //* Validation
+    if ( this.isProd ) {
+      throw new UnauthorizedException('We cannot run SEED on Production!');
+    }
+
     // TODO: Clear Database
     // TODO: Create Users
     // TODO: Create Items
+
     return true;
   }
 
